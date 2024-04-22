471a92ea6f77fca47fcc90b3130848632b61048e
Andy Griffiths <griffiths_andy@icloud.com>
2024-04-22 16:42:02 +0100

    before I try to use bevy_egui

--- src/main_menu/styles.rs
+++ src/main_menu/styles.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+// styles for the 'UI' ;-)
+
 pub const NORMAL_BUTTON_COLOR: Color = Color::rgb(0.15, 0.15, 0.15);
 pub const HOVERED_BUTTON_COLOR: Color = Color::rgb(0.25, 0.25, 0.25);
 pub const PRESSED_BUTTON_COLOR: Color = Color::rgb(0.35, 0.75, 0.35);
